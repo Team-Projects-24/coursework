daea9003fd7b3243de391921cb58cf97f23838d9
Ben Pritchard <benjhpritchard@gmail.com>
2023-03-24 11:53:49 +0000

    TC API updates

--- pages/api/chat/getChatMessages.ts
+++ pages/api/chat/getChatMessages.ts
@@ -1,42 +1,49 @@
-import { NextApiRequest, NextApiResponse } from 'next'
-import { PrismaClient, User } from '@prisma/client'
-import { IUser } from 'types/User.d'
+import { NextApiRequest, NextApiResponse } from "next";
+import { PrismaClient, User } from "@prisma/client";
+import { IUser } from "types/User.d";
+import { IError } from "types/Error.d";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 export default async function handler(
-    req: NextApiRequest,
-    res: NextApiResponse
+  req: NextApiRequest,
+  res: NextApiResponse
 ) {
-    try {
-        const { id } = req.body
-        if (!id) {
-            res.status(400).json({ message: 'Required fields are missing in the request.' })
-            return
-        }
-        const messages = await prisma.message.findMany({
-            where: {
-                chatroomId: id
-            },
-            include: {
-                sender: true
-            }
-        })
-        if (messages) {
-            const chatMessages = messages.map((message) => ({
-                id: message.id,
-                senderId: message.senderId,
-                sender: message.sender,
-                chatroomId: message.chatroomId,
-                content: message.content,
-                createdAt: message.sentAt,
-                updatedAt: message.updatedAt
-            }))
-            res.status(200).json(chatMessages)
-        }
-      }));
-
-
+  try {
+    const errors: string[] = [];
+    const { id } = req.body;
+    if (!id) {
+      errors.push("Required fields are missing in the request.");
     }
-    
-};
+    if (errors.length > 0) {
+      return res.status(400).json({
+        message: "Invalid request error",
+        errors: errors,
+      });
+    }
+    const messages = await prisma.message.findMany({
+      where: {
+        chatroomId: id,
+      },
+      include: {
+        sender: true,
+      },
+    });
+    if (messages) {
+      const chatMessages = messages.map((message) => ({
+        id: message.id,
+        senderId: message.senderId,
+        sender: message.sender,
+        chatroomId: message.chatroomId,
+        content: message.content,
+        createdAt: message.sentAt,
+        updatedAt: message.updatedAt,
+      }));
+      res.status(200).json(chatMessages);
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Server Error" });
+  } finally {
+    await prisma.$disconnect();
+  }
+}

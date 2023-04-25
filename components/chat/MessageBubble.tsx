48d415cbc6835b904f268a3aebe4f21cf60b0ce9
Ben Pritchard <benjhpritchard@gmail.com>
2023-04-25 10:37:26 +0100

    refactor: message bubble variable names

--- components/chat/MessageBubble.tsx
+++ components/chat/MessageBubble.tsx
@@ -1,21 +1,21 @@
-
-interface MessageBubbleProps{
-    text:string;
-    person:string;
-    recOrSen:boolean;
+interface MessageBubbleProps {
+  content: string;
+  userId: string;
+  sent: boolean;
 }
-const MessageBubble: React.FC<MessageBubbleProps> = ({text,person,recOrSen}) =>{
-    const currentTime = new Date().toLocaleTimeString();
-    return(
-        <div>
-         <div className={`speech-bubble ${recOrSen ? 'sent' : 'received'}`}>
-            <p>{text}</p>
-            <span className="time">{currentTime}</span>
-        </div>
-        </div>
-
-    )
-
-    
-}
+const MessageBubble: React.FC<MessageBubbleProps> = ({
+  content,
+  userId,
+  sent,
+}) => {
+  const currentTime = new Date().toLocaleTimeString();
+  return (
+    <div>
+      <div className={`speech-bubble ${sent ? "sent" : "received"}`}>
+        <p>{content}</p>
+        <span className="time">{currentTime}</span>
+      </div>
+    </div>
+  );
+};
 export default MessageBubble;

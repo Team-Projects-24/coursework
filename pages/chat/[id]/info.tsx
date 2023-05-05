340bb00c097317fe8f54f4e1aaf1e3c898d02e80
Redtyre12 <71070151+Redtyre12@users.noreply.github.com>
2023-05-05 14:47:53 +0100

    dropdown functionality+adding member to chat

--- pages/chat/[id]/info.tsx
+++ pages/chat/[id]/info.tsx
@@ -4,13 +4,14 @@ import axios from "axios";
 import Info from "components/chat/info/Info";
 import Members from "components/chat/info/Members";
 import { useRouter } from "next/router";
-import { useState, useEffect } from "react";
+import { useState, useEffect,useRef } from "react";
 import { Message } from "react-hook-form";
 import useUserStore from "stores/userStore";
 import { Button } from "@mui/material";
 import { TextField } from "@mui/material";
-import {Select} from "@mui/material";
+import { Select } from "@mui/material";
 import MembersList from "components/chat/info/Members";
+import { List } from "react-content-loader";
 
 
 
@@ -21,15 +22,37 @@ export default function InfoPage() {
   const user = useUserStore((state) => state.user);
   const [chatData, setChatData] = useState<
     | (Chatroom & {
-        members: User[];
-        messages: Message[];
-      })
+      members: User[];
+      messages: Message[];
+    })
     | null
   >(null);
   const [loading, setLoading] = useState<boolean>(true);
   const [description, setDescription] = useState("");
   const [name, setName] = useState("");
+  const [options, setOptions] = useState<User[]>([]);
+  const [members, setMembers] = useState<User[]>([]);
+  const [newMember, setNewMember] = useState("")
+  const [sUId,ssUId] = useState('');
+
+  useEffect(() => {
+    const fetchOptions = async () => {
+      const response = await axios.get("/api/users/getUsers");
+      setOptions(response.data);
+    };
+
+    fetchOptions();
+  }, []);
+
+  useEffect(() =>{
+    if(chatData){
+      setMembers(chatData.members);
+    }
+  }, [chatData]);
+
   
+
+  const filteredUsers = options.filter((option) => !members.some((member) => member.userId === option.userId));
 
 
   useEffect(() => {
@@ -55,113 +78,161 @@ export default function InfoPage() {
     }
   }, []);
 
-  function CRname(){
-    
-    const handleUpdateE =  async() =>{
+  
+
+  function ADname() {
+
+    const handleUpdateA = async () => {
+      const updatedList = chatData?.members?.map(((person) => person.userId));
       try{
-        const response = await axios.put("/api/chat/" + chatroomId,
-        {
-          name : name as String,
-          description : chatData?.description as String,
-          chatImage : chatData?.chatImage as String,
-          members : chatData?.members?.map((person)=>(person.userId) )
+        const response = await axios.put("/api/chat/" + chatroomId, {
+          name : chatData?.name,
+          description : chatData?.description,
+          chatImage : chatData?.chatImage,
+          members: updatedList
         });
+        
+      } catch (error) {
+        //console.log(error)
+      }
+    }
+
+    handleUpdateA();
+  }
+
+  function CRname() {
+
+    const handleUpdateE = async () => {
+      try {
+        const response = await axios.put("/api/chat/" + chatroomId,
+          {
+            name: name as String,
+            description: chatData?.description as String,
+            chatImage: chatData?.chatImage as String,
+            members: chatData?.members?.map((person) => (person.userId))
+          });
         setName("");
-      }catch(error){
+      } catch (error) {
         console.error(error)
       }
     }
-    
+
     handleUpdateE();
   };
 
-  function CRdesc(){
+  function CRdesc() {
 
-    const handleUpdateD =  async() =>{
-      try{
+    const handleUpdateD = async () => {
+      try {
         const response = await axios.put("/api/chat/" + chatroomId,
-        {
-          name : chatData?.name as String,
-          description : description as String,
-          chatImage : chatData?.chatImage as String,
-          members : chatData?.members?.map((person)=>(person.userId) )
-        });
+          {
+            name: chatData?.name as String,
+            description: description as String,
+            chatImage: chatData?.chatImage as String,
+            members: chatData?.members?.map((person) => (person.userId))
+          });
         setDescription("");
-      }catch(error){
+      } catch (error) {
         console.error(error)
       }
     }
 
     handleUpdateD();
-    
-  };
-  
-function clearingField() {
-  const [inputValue, setInputValue] = useState('');
 
-  const handleClearClick = () => {
-    setInputValue('');
   };
 
-  return (
-    <div>
-      <input type="text" value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
-      <button onClick={handleClearClick}>Clear</button>
-    </div>
-  );
-}
+  function clearingField() {
+    const [inputValue, setInputValue] = useState('');
+
+    const handleClearClick = () => {
+      setInputValue('');
+    };
+
+    async function getUsers() {
+      try {
+        const response = await axios.get("/api/users/getUsers");
+        const users = response.data;
+        return users;
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+
+
+    return (
+      <div>
+        <input type="text" value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
+        <button onClick={handleClearClick}>Clear</button>
+      </div>
+    );
+  }
+
 
   return (
     <>
       <Grid container direction="column">
         <Grid item alignContent={"center"} alignSelf={"center"} xs={12} md={8}>
-          {<Info name={chatData?.name as string} description={chatData?.description as string} chatImage={""} />}
+          {<Info name={chatData?.name as string} description={chatData?.description as string} chatImage={""}/>}
         </Grid>
-        <Grid container direction="column" alignContent ={"center"} alignSelf={"center"} paddingTop={5}>
-        
-      </Grid>
+        <Grid container direction="column" alignContent={"center"} alignSelf={"center"} paddingTop={5}>
+
+        </Grid>
 
         <Grid item
           alignContent={"center"}
           alignSelf={"center"}
           justifyContent={"center"}
           xs={12}
-          md={4}> <Members members={chatData?.members as User[]} />
-        </Grid>
-        <Grid container direction ="row" justifyContent="center" paddingBottom={5} >
-        <FormControl style={{minWidth:230}}>
-        <Select value = {chatData?.members} >
-          <MenuItem value={1}> 1</MenuItem>
-          <MenuItem value={1}> 1</MenuItem>
-          <MenuItem value={1}> 1</MenuItem>
-        </Select>
-
-        </FormControl>
-        
-        <Button variant="contained">Add</Button>
-
-        
-        </Grid>
-      </Grid>
-      
-
-      <Grid container direction="column" alignContent ={"center"} alignSelf={"center"}>  
-        <Grid container direction ="row" justifyContent="center" >
-        <TextField id="proposedDesc" name="proposedDesc" label ="Enter chat description"  variant="outlined" type="text" value={description} onChange={(e) => setDescription(e.target.value)}/>
-        <Button type="submit" variant="contained" onClick={CRdesc}>Submit</Button>
+          md={4}>
+          <Members members={chatData?.members as User[]} />
         </Grid>
 
+        <Grid container direction="row" justifyContent="center" paddingBottom={5} >
+          <FormControl style={{ minWidth: 230 }}>
+            <Select onChange={(e) => setNewMember(e.target.value as string)}
+            id="newMem"
+            MenuProps={{
+              style:{
+                maxHeight: 200,
+              }
+            }}>
+              {filteredUsers.map((option) => (
+                <MenuItem key={option.userId} value={option.userId}>
+                  {option.name}
+                </MenuItem>
+              ))}
 
-        <Grid container direction ="row" justifyContent="center" >
-        <TextField id="proposedName" name="proposedName" label ="Enter New Name"  variant="outlined" type="text" value={name} onChange={(e) => setName(e.target.value)}/>
-        <Button type="submit" variant="contained" onClick={CRname}>Submit</Button>
+
+            </Select>
+
+
+          </FormControl>
+
+          <Button variant="contained" onClick={ADname}>Add</Button>
+
+
         </Grid>
-    
       </Grid>
 
 
+      <Grid container direction="column" alignContent={"center"} alignSelf={"center"}>
+        <Grid container direction="row" justifyContent="center" >
+          <TextField id="proposedDesc" name="proposedDesc" label="Enter chat description" variant="outlined" type="text" value={description} onChange={(e) => setDescription(e.target.value)} />
+          <Button type="submit" variant="contained" onClick={CRdesc}>Submit</Button>
+        </Grid>
 
-      
+
+        <Grid container direction="row" justifyContent="center"  paddingTop={5}>
+          <TextField id="proposedName" name="proposedName" label="Enter New Name" variant="outlined" type="text" value={name} onChange={(e) => setName(e.target.value)} />
+          <Button type="submit" variant="contained" onClick={CRname}>Submit</Button>
+        </Grid>
+
+      </Grid>
+
+
+
+
     </>
   );
 }

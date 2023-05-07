deba29878df80714b71c2161b6391747b42705bd
YahyaDooks <yahyadookanwala@gmail.com>
2023-05-07 17:40:11 +0100

    Some backend progress made, not complete

--- pages/api/admin/getTaskIDs.ts
+++ pages/api/admin/getTaskIDs.ts
@@ -1,2 +1,49 @@
 // api for getting all the task IDs
 
+
+import { NextApiRequest, NextApiResponse } from "next";
+import { PrismaClient, Team, User } from "@prisma/client";
+import { ITeam } from "types/analysis/Team.d";
+
+const prisma = new PrismaClient();
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    const { leaderID } = req.body;
+
+    if (!leaderID) {
+      res
+        .status(400)
+        .json({ message: "Required fields are missing in the request." });
+      return;
+    }
+
+    const results = await prisma.team.findMany({
+      where: {
+        leaderId: leaderID
+      }
+    })
+
+    if (results) {
+
+      let teamIDs: String[] = [];
+
+      results.forEach(result => {
+        teamIDs.push(result.id);
+      })      
+
+      res.status(200).json(teamIDs);
+    } else {
+      res.status(404).json({ message: "Teams not found" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error getting teams" });
+  } finally {
+    await prisma.$disconnect();
+  }
+}
+
--- pages/api/tasks/createTask.ts
+++ pages/api/tasks/createTask.ts
@@ -1,69 +1,54 @@
-import { PrismaClient } from "@prisma/client";
-import type { NextApiRequest, NextApiResponse } from "next";
+//api for creating task
+
+import { NextApiRequest, NextApiResponse } from "next";
+import { Chatroom, User } from "@prisma/client";
+// import { ICreateChatroom } from "types/Chatroom.d";
 import { ICreateTask } from "types/Task.d";
-import {
-  sendErrorResponse,
-  sendSuccessResponse,
-  sendBadRequestResponse,
-} from "../responses";
+import prisma from "lib/prisma";
+import { ICreateChatroom } from "types/Chatroom.d";
 
-const prisma = new PrismaClient();
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { room } = req.body;
 
-/**
- * @author Tom Whitticase and Ben Pritchard
- *
- * @description Adds a new task to the database.
- *
- * @input {INewTask} newTask - data for the new task
- *
- * @output {status: 200, message: "Task created successfully"} on success
- * @output {status: 400, message: "Missing required fields"} if required fields are missing
- * @output {status: 500, message: "Error creating task"} if there was an error creating the task
- */
-
-export default async (req: NextApiRequest, res: NextApiResponse) => {
-  // Destructure the expected properties from the request body
-  const task: ICreateTask = req.body.task;
-  // Check if any of the required properties are missing
-  if (!task) {
-    // If any of the required properties are missing, return a 400 Bad Request response
-    return sendBadRequestResponse(res, { message: "Missing required fields" });
+  switch (req.method) {
+    case "POST":
+      return handlePost(room, res);
+    default:
+      res.setHeader("Allow", ["POST"]);
+      res.status(405).end(`Method ${req.method} Not Allowed`);
   }
+}
 
+// POST /api/chat
+async function handlePost(room: ICreateChatroom, res: NextApiResponse) {
   try {
-    // Create a new task in the database
-    const mapStatus = {
-      completed: "COMPLETED",
-      "in-progress": "INPROGRESS",
-      review: "REVIEW",
-      "to-do": "TODO",
-    };
-    const addedTask = await prisma.task.create({
+    const chat = await prisma.chatroom.create({
       data: {
-        name: task.name,
-        description: task.description,
-        status: mapStatus[task.status] as
-          | "COMPLETED"
-          | "INPROGRESS"
-          | "TODO"
-          | "REVIEW",
-        deadline: task.deadline,
-        manHours: task.manHours,
-        subTasks: JSON.parse(JSON.stringify(task.subTasks)),
-        assigneeId: task.userId,
-        projectId: task.projectId,
+        name: room.name,
+        creatorId: room.creatorId,
+        description: "",
+        private: room.private,
+        chatImage: room.chatImage,
+        members: {
+          connect: [
+            { userId: room.creatorId },
+            ...room.members.map((memberId) => ({ userId: memberId })),
+          ],
+        },
       },
     });
-
-    // If the task was created successfully, return a 200 Success response
-    return sendSuccessResponse(res, {
-      message: "Task created successfully",
-    });
+    if (chat) {
+      res.status(200).json(chat);
+    } else {
+      res.status(404).json({ message: "Chat not created" });
+    }
   } catch (error) {
-    // If there was an error creating the task, return a 500 Internal Server Error response
-    return sendErrorResponse(res, { message: "Error creating task" });
+    console.error(error);
+    res.status(500).json({ message: "Error making chat" });
   } finally {
-    // Disconnect from the database, regardless of whether there was an error or not
     await prisma.$disconnect();
   }
-};
+}

5b60d1f2c3bdef961160383814cb0f7b444ff7a0
Ben Pritchard <benjhpritchard@gmail.com>
2023-05-09 18:55:08 +0100

    chore: removed commented code

--- server.js
+++ server.js
@@ -23,34 +23,3 @@ var PORT = 3001;
 httpServer.listen(PORT, function () {
   console.log("Server running on port ".concat(PORT));
 });
-// import express, { Express, Request, Response } from "express";
-// import * as http from "http";
-// import next, { NextApiHandler } from "next";
-// import * as socketio from "socket.io";
-// const port: number = parseInt(process.env.PORT || "3000", 10);
-// const dev: boolean = process.env.NODE_ENV !== "production";
-// const nextApp = next({ dev });
-// const nextHandler: NextApiHandler = nextApp.getRequestHandler();
-// nextApp.prepare().then(async () => {
-//   const app: Express = express();
-//   const server: http.Server = http.createServer(app);
-//   const io: socketio.Server = new socketio.Server();
-//   io.attach(server);
-//   app.get("/hello", async (_: Request, res: Response) => {
-//     res.send("Hello World");
-//   });
-//   io.on("connection", (socket: socketio.Socket) => {
-//     console.log("connection");
-//     socket.emit("status", "Hello from Socket.io");
-//     socket.on("send-message", (message: string) => {
-//       io.emit("receive-message", message);
-//     });
-//     socket.on("disconnect", () => {
-//       console.log("client disconnected");
-//     });
-//   });
-//   app.all("*", (req: any, res: any) => nextHandler(req, res));
-//   server.listen(port, () => {
-//     console.log(`> Ready on http://localhost:${port}`);
-//   });
-// });
